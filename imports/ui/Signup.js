c49b5271b07f2565be465dcc2c2208a9bfbba033
William Hernandez Sierra <whs465@gmail.com>
2017-06-26 11:37:51 +0630

    Change language form signup

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -40,7 +40,7 @@ export default class Signup extends React.Component {
 						<input type="password" ref="password" name="password" placeholder="Password" />
 						<button className="button">Create Account</button>
 					</form>
-					<Link to="/">Already have an account?</Link>
+					<Link to="/">Have an account?</Link>
 
 				</div>
 			</div>
